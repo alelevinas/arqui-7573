fe1c1cd8f67b66fe91c296d9be2dff8adb10c710
alejandro <levinasale@gmail.com>
2017-11-29 23:31:42 -0300

    Added artillery yaml and new configurations

--- gunicorn/app.py
+++ gunicorn/app.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from redis import Redis
+from flask import send_file
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
+
+static_dir = '/home/www/arqui/static/files'
 
 @app.route('/')
 def hello():
     count = redis.incr('hits')
     return 'Hello World from Docker!!!!! I have been seen {} times.\n'.format(count)
+
+@app.route("/static/<string:filename>")
+def serve_static(filename):
+  return send_file('{}/{}'.format(static_dir, filename))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
